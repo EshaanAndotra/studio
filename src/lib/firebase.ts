bfb7bd623ed829e31f79460adf995035db8695ef
Bashab Mazumder <bashab.mazumder@gmail.com>
2025-09-08 20:34:10 +0000

    enable offline persistence

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 
 const firebaseConfig = {
   projectId: 'm-health-jxug7',
@@ -16,6 +16,20 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+try {
+  enableIndexedDbPersistence(db);
+} catch (error: any) {
+  if (error.code == 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled
+    // in one tab at a time.
+    console.warn('Firestore persistence failed: multiple tabs open.');
+  } else if (error.code == 'unimplemented') {
+    // The current browser does not support all of the
+    // features required to enable persistence
+    console.warn('Firestore persistence is not available in this browser.');
+  }
+}
 
 
 export { app, auth, db };
